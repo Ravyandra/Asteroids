bc4e33fd39023bafff478e010b632b628abbd2f6
Ravyandra <gaming@ravyandra.com>
2025-02-02 22:33:33 +0100

    Defined CircleChape class & inherited to player class, called player in main.py & draws player character

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame
 from constants import *
-
+from player import *
 
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    frames_clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = frames_clock.tick(60) / 1000
 
     print("Starting asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
